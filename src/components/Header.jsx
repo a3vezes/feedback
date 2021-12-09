9aede09eafe68c4c7eb042c69f98c70893148189
Arthur <tucaariza@gmail.com>
2021-12-09 20:15:51 -0300

    Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types'
 
-const Header = ({text}) => {
+const Header = ({ text, bgColor, textColor }) => {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
     return (
-        <header>
+        <header style={headerStyles}>
             <div className="container">
                 <h2>{text}</h2>    
             </div>   
@@ -11,11 +15,15 @@ const Header = ({text}) => {
 }
 
 Header.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 }
 
 export default Header
